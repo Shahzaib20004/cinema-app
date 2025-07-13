3ac34308831313e6e644d0b0c0387ab35f26e8a6
shahzaib20004 <shahzaibsanaullah75@gmail.com>
2025-07-13 22:30:21 +0500

    Remove SignUpPage import and route to fix build

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import CinemaPage from "./pages/CinemaPage";
 import MoviePage from "./pages/MoviePage";
 import SeatSelectionPage from "./pages/SeatSelectionPage";
-import UserFormPage from "./pages/userformpage";
+import UserFormPage from "./pages/userformpage"; // file name must match exactly
 import CheckoutPage from "./pages/CheckoutPage";
 import ConfirmationPage from "./pages/ConfirmationPage";
 import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
-
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Footer from "./components/Footer";
 
 import "./App.css";
@@ -23,16 +20,14 @@ function App() {
         <Navbar />
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/cinema" element={<CinemaPage />} />
           <Route path="/movies" element={<MoviePage />} />
           <Route path="/seats" element={<SeatSelectionPage />} />
-<Route path="/userform" element={<userformpage />} />
+          <Route path="/userform" element={<UserFormPage />} />
           <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/confirmation" element={<ConfirmationPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-        </Routes>
+          <Route path="/login" element={<LoginPage />} />        </Routes>
 
         <Footer />
       </div>
